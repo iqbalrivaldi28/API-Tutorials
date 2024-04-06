6e9a9db717278b414912fdb6791c24b320af0016
M. Iqbal Rivaldi <rivaldiiqbal921@gmail.com>
2024-04-06 14:36:19 +0700

    feat: Upload API Tutorials

---
+++ src/main/java/com/iqbal/tutorial/model/Tutorial.java
@@ -0,0 +1,29 @@
+package com.iqbal.tutorial.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tutorials")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "published")
+    private boolean published;
+
+}
